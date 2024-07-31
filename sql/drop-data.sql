5abb85eb56362998e4bb9d58bdcc81d65a6b881a
chienaeae <chienaeae@gmail.com>
2024-07-31 16:21:12 -0400

    add drop scripts

---
+++ sql/drop-data.sql
@@ -0,0 +1,20 @@
+-- Drop the Payments table
+DROP TABLE Payments CASCADE CONSTRAINTS;
+
+-- Drop the OrderItems table
+DROP TABLE OrderItems CASCADE CONSTRAINTS;
+
+-- Drop the Orders table
+DROP TABLE Orders CASCADE CONSTRAINTS;
+
+-- Drop the Inventory table
+DROP TABLE Inventory CASCADE CONSTRAINTS;
+
+-- Drop the Administrators table
+DROP TABLE Administrators CASCADE CONSTRAINTS;
+
+-- Drop the Products table
+DROP TABLE Products CASCADE CONSTRAINTS;
+
+-- Drop the Customers table
+DROP TABLE Customers CASCADE CONSTRAINTS;
